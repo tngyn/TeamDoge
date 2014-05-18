b61c6157fbb0751b4ada63370c2ef42a51a3bee4
tngyn <timngyn@gmail.com>
2014-05-17 18:22:07 -0700

    Removing keyboard from main activity

--- RestaurantApp/src/com/teamdoge/restaurantapp/MainActivity.java
+++ RestaurantApp/src/com/teamdoge/restaurantapp/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -40,6 +41,10 @@ public class MainActivity extends FragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		getWindow().setSoftInputMode(
+			      WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+		
 
 		mTitle = mDrawerTitle = getTitle();
 		mDrawerSections = getResources().getStringArray(R.array.drawer_sections_array);
